0f013aae6c664208768006bd1f177027edc4c848
Anton Maminov <anton.linux@gmail.com>
2015-05-05 18:01:27 +0300

    skeleton for rspec

--- Rakefile
+++ Rakefile
@@ -1,1 +1,17 @@
+require 'rake/testtask'
 require "bundler/gem_tasks"
+
+begin
+  require 'rspec/core/rake_task'
+rescue LoadError
+end
+
+if defined?(RSpec)
+  desc "Run specs"
+  RSpec::Core::RakeTask.new("spec") do |t|
+    t.pattern = "spec/core/**/*_spec.rb"
+  end
+
+  desc 'Default: run specs.'
+  task default: 'spec'
+end
---
+++ spec/core/spec_helper.rb
@@ -0,0 +1,19 @@
+require 'freshdesk_api'
+
+def client
+  credentials = File.join(File.dirname(__FILE__), '..', 'fixtures', 'credentials.yml')
+  @client ||= begin
+    client = FreshdeskAPI::Client.new do |config|
+      if File.exists?(credentials)
+        data = YAML.load(File.read(credentials))
+        config.username = data["username"]
+        config.password = data["password"]
+        config.url = data["url"]
+      else
+        STDERR.puts "add your credentials to spec/fixtures/credentials.yml (see: spec/fixtures/credentials.yml.example)"
+      end
+    end
+
+    client
+  end
+end
